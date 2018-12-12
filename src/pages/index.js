b7326fd1c2be4be598eab9b466533369526235fd
dbertella <daniele.bertella@gmail.com>
2018-12-12 09:00:13 +0000

    Add next birthday sentence

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import {
+  closestTo,
+  format,
+  setYear,
+  isBefore,
+  startOfDay,
+  addYears,
+  distanceInWords,
+} from 'date-fns'
+import itLocale from 'date-fns/locale/it'
 import Layout from '../components/layout'
 import { Family } from '../components/family'
 import { setConfig } from 'react-hot-loader'
@@ -13,11 +23,53 @@ const lookupMemberInTime = (name, year) => {
   const [memberYear] = memberInfo.bornDate.split('-')
   return memberYear <= year ? name : 'backcard'
 }
+const today = startOfDay(new Date())
+const currentYear = today.getFullYear()
+const memebersByDate = Object.keys(members).reduce((acc, key) => {
+  const birthday = setYear(members[key].bornDate, currentYear)
+  const nextBirthday = format(
+    isBefore(birthday, today) ? addYears(birthday, 1) : birthday,
+    'YYYY-MM-DD'
+  )
+  if (acc.hasOwnProperty(nextBirthday)) {
+    acc[nextBirthday].push({
+      ...members[key],
+      picture: key,
+    })
+  } else {
+    acc[nextBirthday] = [
+      {
+        ...members[key],
+        picture: key,
+      },
+    ]
+  }
+  return acc
+}, {})
+const nextBirthday = format(
+  closestTo(
+    today,
+    Object.keys(memebersByDate).map(date => {
+      const birthday = setYear(date, currentYear)
+      return isBefore(birthday, today) ? (birthday, 1) : birthday
+    })
+  ),
+  'YYYY-MM-DD'
+)
+
 const IndexPage = () => {
   const [year, setYear] = React.useState('2018')
   const lookupMember = name => lookupMemberInTime(name, year)
   return (
     <Layout year={year} setYear={setYear}>
+      <div>
+        <h2>
+          Il prossimo compleanno è di{' '}
+          {memebersByDate[nextBirthday].map(member => member.name).join(' e ')}{' '}
+          {distanceInWords(today, nextBirthday, { locale: itLocale, addSuffix: true })}{' '}
+          🎉🎉🎉
+        </h2>
+      </div>
       <Family
         familyName="Western"
         members={[
